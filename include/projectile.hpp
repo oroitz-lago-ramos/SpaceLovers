8c6886dde6937d4ec7d20f6b94e262d831c13e8b
rle-ru <lerurodolpe@yahoo.fr>
2023-08-08 13:17:03 +0200

    projectiles destroying and moving both direction

--- include/projectile.hpp
+++ include/projectile.hpp
@@ -7,10 +7,11 @@
 
 class Projectile : public Character
 {
-	Projectile(int r, int g, int b, SDL_Rect rect);
+	Projectile(int r, int g, int b, SDL_Rect rect, int direction);
 	~Projectile();
 	
 	void update();
+	int direction;
 };
 
 #endif
