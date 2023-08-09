a1fb21c728473d73973fff5598e52c4d072d2ea2
rle-ru <lerurodolpe@yahoo.fr>
2023-08-09 16:17:51 +0200

    TTF_Init

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -15,20 +15,20 @@ std::set<Button *> Graphics::buttons = {};
 
 Graphics::Graphics()
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    Graphics::window = SDL_CreateWindow("C++", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, this->windowWidth, this->screenHeight, SDL_WINDOW_SHOWN);
-    Graphics::renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED);
-    TTF_Init();
+	SDL_Init(SDL_INIT_EVERYTHING);
+	Graphics::window = SDL_CreateWindow("C++", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, this->windowWidth, this->screenHeight, SDL_WINDOW_SHOWN);
+	Graphics::renderer = SDL_CreateRenderer(this->window, -1, SDL_RENDERER_ACCELERATED);
+	TTF_Init();
 }
 
 Graphics::~Graphics()
 {
-    for (auto button : std::set<Button *>(Graphics::buttons))
-    {
-        button->~Button();
-    }
-    SDL_DestroyRenderer(Graphics::renderer);
-    SDL_DestroyWindow(Graphics::window);
-    TTF_Quit();
-    SDL_Quit();
+	for (auto button : std::set<Button *>(Graphics::buttons))
+	{
+		button->~Button();
+	}
+	SDL_DestroyRenderer(Graphics::renderer);
+	SDL_DestroyWindow(Graphics::window);
+	TTF_Quit();
+	SDL_Quit();
 }
