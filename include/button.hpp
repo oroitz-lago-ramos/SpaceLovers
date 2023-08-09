fdd7b07825f71c92bb7b5ce963599cac56751822
Sluchai40 <mabbolos8@gmail.com>
2023-08-09 16:06:15 +0200

    font pour button

--- include/button.hpp
+++ include/button.hpp
@@ -8,14 +8,16 @@
 class Button : public Entity
 {
 public:
-    Button(int r, int g, int b, int x, int y, int width, int height, std::function<void()> onClick, const char *message);
-    Button(int r, int g, int b, int x, int y, int width, int height, std::function<void()> onClick, const char *message, bool add);
-    ~Button();
+	Button(int r, int g, int b, int x, int y, int width, int height, std::function<void()> onClick, const char *message);
+	Button(int r, int g, int b, int x, int y, int width, int height, std::function<void()> onClick, const char *message, bool add);
+	~Button();
 
-    std::function<void()> onClick;
-    const char *message;
+	char *message;
 
-    void text();
+	std::function<void()> onClick;
+	const char *message;
+
+	void text();
 };
 
 #endif
