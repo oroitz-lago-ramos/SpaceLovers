15225fd6e5392bfba00327ef4366542b1137697a
Sluchai40 <mabbolos8@gmail.com>
2023-08-10 14:39:29 +0200

    ca marche pas rodolphe attaque

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -9,17 +9,17 @@
 class Graphics
 {
 public:
-    Graphics();
-    ~Graphics();
-    static SDL_Window *window;
-    static SDL_Renderer *renderer;
+	Graphics();
+	~Graphics();
+	static SDL_Window *window;
+	static SDL_Renderer *renderer;
 
-    static int screenWidth;
-    static int screenHeight;
-    static int windowWidth;
+	static int screenWidth;
+	static int screenHeight;
+	static int windowWidth;
 
-    // Include de "set" afin de créer un tableau de tous les (Button *) le tableau s'appelle buttons
-    static std::set<Button *> buttons;
+	// Include de "set" afin de créer un tableau de tous les (Button *) le tableau s'appelle buttons
+	static std::set<Button *> buttons;
 };
 
 #endif
--- src/level.cpp
+++ src/level.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <set>
+#include <iostream>
 
 #include "level.hpp"
 #include "enemy.hpp"
