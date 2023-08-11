508e94701f9c4be41710bbacde56c3b695eac724
rle-ru <lerurodolpe@yahoo.fr>
2023-08-11 12:04:02 +0200

    Removed legacy comments

--- include/skillTree.hpp
+++ include/skillTree.hpp
@@ -21,43 +21,4 @@ public:
 	void render();
 };
 
-// class SkillNode : public Entity
-// {
-// public:
-// 	SkillNode(int r, int g, int b, std::string name);
-// 	~SkillNode();
-
-// 	void addRequirement(SkillNode *requirement);
-// 	void setPosition(int x, int y);
-// 	std::string name;
-
-// private:
-// 	std::vector<SkillNode *> requirements;
-// };
-
-// class Entity
-// {
-// public:
-//     Entity(int r, int g, int b, int x, int y, int width, int height);
-//     ~Entity();
-//     void render();
-//     void setX(float x);
-//     float getX();
-//     void setY(float y);
-//     float getY();
-
-//     int r;
-//     int g;
-//     int b;
-//     int width;
-//     int height;
-//     SDL_Rect rect;
-//     SDL_Texture* texture;
-
-// private:
-//     float x;
-//     float y;
-
-// };
-
 #endif
